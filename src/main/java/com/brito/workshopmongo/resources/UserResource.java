6753d479b457f8620a8626632bffa32c652eb56f
Matheus Brito <mathhbrito@gmail.com>
2025-01-05 23:19:00 -0300

    padrão DTO

--- src/main/java/com/brito/workshopmongo/resources/UserResource.java
+++ src/main/java/com/brito/workshopmongo/resources/UserResource.java
@@ -8,21 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brito.workshopmongo.domain.User;
+import com.brito.workshopmongo.dto.UserDTO;
 import com.brito.workshopmongo.services.UserService;
 
 import ch.qos.logback.core.joran.spi.HttpUtil.RequestMethod;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll () {		
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
-	
+
 }
