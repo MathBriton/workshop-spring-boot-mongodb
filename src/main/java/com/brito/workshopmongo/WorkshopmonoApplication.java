0b2e98d612c7e0c110d87b62711fcfe5df3fa590
Matheus Brito <mathhbrito@gmail.com>
2025-01-05 22:26:11 -0300

    initial

---
+++ src/main/java/com/brito/workshopmongo/WorkshopmonoApplication.java
@@ -0,0 +1,13 @@
+package com.brito.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brito/workshopmongo/WorkshopmonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brito.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
